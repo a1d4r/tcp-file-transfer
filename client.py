3e02cf345edbcd5f01a4911d275fdd8c915b4ab2
Aidar Garikhanov <a1d4r@yandex.ru>
2020-09-16 22:09:19 +0300

    Add command line arguments to the client program

--- client.py
+++ client.py
@@ -1,9 +1,16 @@
 import socket
 from pathlib import Path
+import argparse
 
-HOST = '127.0.0.1'
-PORT = 5000
-FILEPATH = Path('./file.txt')
+parser = argparse.ArgumentParser(description="Transfer a file to a server")
+parser.add_argument('file', type=str, help='path to the file')
+parser.add_argument('server', type=str, help='domain name or ip address of the server')
+parser.add_argument('port', type=int, help='port number')
+
+args = parser.parse_args()
+HOST = args.server
+PORT = args.port
+FILEPATH = Path(args.file)
 
 
 print('Connecting...')
@@ -16,3 +23,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
 
     sock.sendall((FILEPATH.name + '\n\n').encode())
     sock.sendall(data)
+
+print('File has been successfully transfered.')
