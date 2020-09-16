412c361925cae9ff59fe9428267b5341d3718a16
Aidar Garikhanov <a1d4r@yandex.ru>
2020-09-16 22:46:15 +0300

    Add comment and docstrings

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 from pathlib import Path
 import argparse
 
+# Command line arguments
 parser = argparse.ArgumentParser(description="Transfer a file to a server")
 parser.add_argument('file', type=str, help='path to the file')
 parser.add_argument('server', type=str, help='domain name or ip address of the server')
@@ -15,12 +16,15 @@ FILEPATH = Path(args.file)
 
 print('Connecting...')
 
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    # Establish TCP connection with the server
     sock.connect((HOST, PORT))
     print('Connection has been established.')
+    # Read file content
     with open(FILEPATH, 'rb') as f:
         data = f.read()
-
+    # Send file name and its content to the socket
     sock.sendall((FILEPATH.name + '\n').encode())
     sock.sendall(data)
 
--- server.py
+++ server.py
@@ -1,25 +1,34 @@
 import asyncio
 from pathlib import Path
+import argparse
 import os
 
-
+# Directory for storing uploaded files
 FILES_DIR = Path(__file__).parent.absolute() / 'server_files'
+# Create directory if it's not exist
 FILES_DIR.mkdir(parents=True, exist_ok=True)
 
+PORT = 5000
 
 class FileWriter:
-
+    """Class for writing raw data to files."""
     def write(self, data):
+        """Write binary data to a file. 
+        The name of the file and its content is stored in data separated by EOL."""
         filename, data = data.split(b'\n', 1)
         filename = self.avoid_filename_collisions(filename.decode())
         with open(FILES_DIR / filename, 'wb') as f:
             f.write(data)
 
     def avoid_filename_collisions(self, filename):
+        """Return a filename which won't collide with existing files."""
+        # If there is no such file yet the specified filename is ok
         if not (FILES_DIR / filename).exists():
             return filename
+        # Split filename into name and extension
         name, ext = os.path.splitext(filename)
         new_filename = f'{name}_copy{ext}'
+        # Find suitable filename for the copy
         copy_number = 0
         while (FILES_DIR / new_filename).exists():
             copy_number += 1
@@ -28,7 +37,7 @@ class FileWriter:
 
 
 class FileTransferProtocol(asyncio.Protocol):
-
+    """TCP transfer protocol"""
     def __init__(self):
         self.filewriter = FileWriter()
         super().__init__()
@@ -46,4 +55,4 @@ async def main(host, port):
     await server.serve_forever()
 
 
-asyncio.run(main('', 5000))
+asyncio.run(main('', PORT))
